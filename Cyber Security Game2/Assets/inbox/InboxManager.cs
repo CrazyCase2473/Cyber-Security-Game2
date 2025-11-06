9a5c74640dbe33f8df1cb88759df54524e696cef
CrazyCase2473 <deacon.mc08@gmail.com>
2025-11-05 19:07:38 -0600

    started inbox

---
+++ Cyber Security Game2/Assets/inbox/InboxManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NUnit.Framework;
+
+public class InboxManager : MonoBehaviour
+{
+    public GameObject emailPrefab;
+    public Transform emailListParent;
+    public float emailInterval = 5f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
