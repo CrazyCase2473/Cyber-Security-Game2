2336f0e2e3fc248125bd9a4181dfd35022030781
CrazyCase2473 <deacon.mc08@gmail.com>
2025-11-13 15:04:12 -0600

    email data scriptable object made
    coded scriptable object for email data to be used in email prefab soon

--- Cyber Security Game2/Assets/inbox/EmailData.cs
+++ Cyber Security Game2/Assets/inbox/EmailData.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-[System.Serializable]
-public class EmailData
+[CreateAssetMenu(fileName = "EmailData", menuName = "Email System/EmailData")]
+public class EmailData : ScriptableObject
 {
     public string sender;
     public string subject;
-    public string message;
+    [TextArea(3, 10)] public string message;
     public bool isPhishing;
 }
