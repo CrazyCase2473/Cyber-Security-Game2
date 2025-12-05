00d0f4b728c1a8b6a0e7968d26b14f4b9e193ca1
CrazyCase2473 <deacon.mc08@gmail.com>
2025-12-05 15:18:24 -0600

    added question to deacon scene

--- Cyber Security Game2/Assets/Scripts/Bug.cs
+++ Cyber Security Game2/Assets/Scripts/Bug.cs
@@ -2,11 +2,41 @@ using UnityEngine;
 
 public class Bug : MonoBehaviour
 {
-    public float speed = 2f;
+    public float speed = 3f;
+    public float moveRadius = 5f;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        PickNewTarget();
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if ((transform.position - targetPosition).sqrMagnitude < 0.01f)
+        {
+            PickNewTarget();
+        }
+    }
+
+    void PickNewTarget()
+    {
+        Vector3 newTarget;
+        int attempts = 0;
+
+        do
+        {
+            float randomX = transform.position.x + Random.Range(-moveRadius, moveRadius);
+            float randomY = transform.position.y + Random.Range(-moveRadius, moveRadius);
+            newTarget = new Vector3(randomX, randomY, transform.position.z);
+            attempts++;
+        }
+        // Check that no other bug is too close to this target
+        while (Physics2D.OverlapCircle(newTarget, 0.5f) != null && attempts < 10);
+
+        targetPosition = newTarget;
     }
 
     void OnTriggerEnter2D(Collider2D other)
